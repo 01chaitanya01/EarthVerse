8ff41d7b10ac0e21141979c9ffaa836559b0950f
chaitanya choudhari <100710855+01chaitanya01@users.noreply.github.com>
2023-03-03 21:45:14 +0530

    navbar close solve, linkedin added, about section update

--- navbar.js
+++ navbar.js
@@ -1,19 +1,11 @@
 var subMenu = document.getElementById("subMenu");
 
 function showMenu(){
-    // subMenu.style.display="block";
-    // subMenu.style.right = "0px";
-subMenu.style.height = "30vh";
-    // if(subMenu.style.max==="0vh"){ 
-    //     subMenu.style.max="30vh";
-    // }
-    // else{
-    //     subMenu.style.max="0vh";
-    // }
+if(subMenu.style.height ==="0vh"){
+  subMenu.style.height = "25vh";
 }
-function hideMenu(){
-    // subMenu.style.right = "-201px";
-    // subMenu.style.display ="none";
-    subMenu.style.height = "0vh";
+else{
+  subMenu.style.height = "0vh"
+}
 }
 
