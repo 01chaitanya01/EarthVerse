65ba81ea88c0c94d789842087e08159425c8e55d
chaitanya choudhari <100710855+01chaitanya01@users.noreply.github.com>
2023-03-03 19:32:24 +0530

    added all required files

---
+++ navbar.js
@@ -0,0 +1,19 @@
+var subMenu = document.getElementById("subMenu");
+
+function showMenu(){
+    // subMenu.style.display="block";
+    // subMenu.style.right = "0px";
+subMenu.style.height = "30vh";
+    // if(subMenu.style.max==="0vh"){ 
+    //     subMenu.style.max="30vh";
+    // }
+    // else{
+    //     subMenu.style.max="0vh";
+    // }
+}
+function hideMenu(){
+    // subMenu.style.right = "-201px";
+    // subMenu.style.display ="none";
+    subMenu.style.height = "0vh";
+}
+
